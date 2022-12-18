a0563bb3563836f5a00d36b952e7eb0d0179eeba
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2022-12-18 18:30:36 -0300

    Inserindo o arquivo ao repositório

---
+++ criarDocker.sh
@@ -0,0 +1,14 @@
+apt update -y
+apt upgrade -y
+
+docker build -t siteTeste
+docker run siteTeste
+
+cd /var/lib/docker/volumes/siteTeste/_data
+apt install apache 2 -y
+
+cd /var/www/html 
+wget https://github.com/denilsonbonatti/toshiro-shibakita/archive/refs/heads/main.zip
+unzip main.zip -y
+
+echo "Acessar de outro computador ou no navegador do pc"
