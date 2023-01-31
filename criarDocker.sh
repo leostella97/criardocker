d91afb3a9b5ace10e370b7c247a7f28db28509cd
Leonardo Stella <leonstella97@gmail.com>
2023-01-31 14:29:04 -0300

    Correção de erros

--- criarDocker.sh
+++ criarDocker.sh
@@ -1,11 +1,11 @@
 apt update -y
 apt upgrade -y
-
+ 
 docker build -t siteTeste
 docker run siteTeste
 
 cd /var/lib/docker/volumes/siteTeste/_data
-apt install apache 2 -y
+apt pull httpd -y
 
 cd /var/www/html 
 wget https://github.com/denilsonbonatti/toshiro-shibakita/archive/refs/heads/main.zip
